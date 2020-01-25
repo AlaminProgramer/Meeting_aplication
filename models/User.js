3db13a4e0c15ef768b21a04255862f2acdbdfdce
alamin <alaminpublicgithub@gmail.com>
2020-01-25 18:38:32 +0600

    push to  git

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+const userSchema=new Schema({
+	userId:{
+		type:String
+	},
+	username:{
+		type:String,
+		require:true
+	},
+	email:{
+		type:String,
+		require:true
+	},
+	role:{
+		type:String
+	},
+	password:{
+		type:String,
+		require:true
+	},
+	isApproved:{
+		type:Boolean
+	},
+	date:{
+		type:Date,
+		default:Date.now
+	}
+},{
+	collection:"users"
+})
+
+module.exports=User=mongoose.model('users',userSchema);
